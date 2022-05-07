0e9cf56ede9272175509ae730fc79d7c01cf654a
poommin2543 <57926799+poommin2543@users.noreply.github.com>
2022-05-07 21:03:34 +0700

    Initial commit

---
+++ src/ExData.js
@@ -0,0 +1,16 @@
+const workHistory = [
+    {
+        companyName : 'sony technology (thailand) co. ltd',
+        role: 'cooperative student',
+        duration: "Sep'21 – March'22",
+        workDes: [
+            'Manufacturing Design Department.',
+            ' Design and developed programs to support the production process.',
+        ],
+        exposer: ["C#", "Python"] ,
+        link: 'https://www.sony.co.th/microsite/companyoutline/th/',
+    },
+    
+];
+
+export default workHistory;
---
+++ src/root.link.js
@@ -0,0 +1,11 @@
+export const RESUME_PATH = 'https://drive.google.com/';
+export const GITHUB_USERNAME = 'codewithvk';
+export const LINKEDIN_USERNAME = 'vivekkumarjaviya';
+export const FIRST_NAME = 'Poommin';
+export const LAST_NAME = 'Phinphimai';
+export const ADDRESS = 'Rajkot, Gujrat'
+export const EMAIL_ID = 'poommin2543@gmail.com';
+export const TWITTER_ID = 'Vk_javiya';
+export const FRONT_PAGE_NAME = 'Poommin Phinphimai';
+export const FRONT_PAGE_DESC = 'Programming  & Support Manufacturing Design Department.I like to build things solving problems.'
+
